20c5afc1b083b083b22d2184189a3e9b2feeada9
Erasumus <Erasumus@mail.ru>
2016-11-07 19:29:39 +0300

    Autotest
    sdasd

---
+++ avtoTest1/UnitTest1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class Untitled
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://kpfu.ru/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheUntitledTest()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/");
+            driver.FindElement(By.LinkText("Вход")).Click();
+            driver.FindElement(By.Name("p_login")).Clear();
+            driver.FindElement(By.Name("p_login")).SendKeys("");
+            driver.FindElement(By.Name("p_pass")).Clear();
+            driver.FindElement(By.Name("p_pass")).SendKeys("bn1bn1bb2");
+            driver.FindElement(By.Name("p_login")).Clear();
+            driver.FindElement(By.Name("p_login")).SendKeys("erasumus@mail.ru");
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+            driver.FindElement(By.XPath("//a[2]/span")).Click();
+            driver.FindElement(By.Name("p_phone")).Clear();
+            driver.FindElement(By.Name("p_phone")).SendKeys("89063085846");
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
